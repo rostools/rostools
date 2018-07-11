6236f0b221dbddadb1bcb05f7b93db9cc5f4be77
Luke W Johnston <lwjohnst@gmail.com>
2018-07-11 16:11:41 +0200

    import from prodigenr

---
+++ R/utils.R
@@ -0,0 +1,16 @@
+
+#' Project directory generator
+#'
+#' See the documentation from the \link[prodigenr]{prodigenr} package for details:
+#'
+#' - \code{prodigenr::\link[prodigenr]{setup_project}}.
+#' - \code{prodigenr::\link[prodigenr]{create_slides}}.
+#' - \code{prodigenr::\link[prodigenr]{create_poster}}.
+#' - \code{prodigenr::\link[prodigenr]{create_manuscript}}.
+#' - \code{prodigenr::\link[prodigenr]{create_abstract}}.
+#'
+#' @name setup_project
+#' @rdname setup_project
+#' @export
+#' @importFrom prodigenr setup_project create_slides create_poster create_manuscript create_abstract
+NULL
