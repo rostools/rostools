44098f6ab12ca9ee7480fd9d89af39c7f50e3600
Luke W Johnston <lwjohnst@gmail.com>
2018-07-11 16:11:31 +0200

    remove include_ funs that were part of prodigenr

--- R/includes.R
+++ R/includes.R
@@ -1,20 +1,4 @@
 
-include_readmes <- function() {
-    copy_template_file('README.md')
-    fs::dir_create("doc")
-    copy_template_file('doc-README.md', file.path("doc", "README.md"))
-    fs::dir_create("data")
-    copy_template_file('data-README.md', file.path("data", "README.md"))
-    copy_template_file("R-README.md", file.path("R", "README.md"))
-    done('Adding several ', value('README.md'), ' files to the project.')
-}
-
-include_r_files <- function() {
-    copy_template_file("setup.R", file.path("R", "setup.R"))
-    copy_template_file("fetch_data.R", file.path("R", "fetch_data.R"))
-    done('Adding some basic R functions to the ', value('R/'), ' folder.')
-}
-
 #' License the project under MIT.
 #'
 #' @param path Path to the project.
@@ -71,3 +55,4 @@ include_strobe <- function(path = '.') {
     utils::capture.output(cat(strobe, file = file_path))
     invisible()
 }
+
