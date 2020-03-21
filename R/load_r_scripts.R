a4a5ff76439eef66f901cdb8c412f624344446da
Luke Johnston <lwjohnst@gmail.com>
2020-03-21 22:57:52 +0100

    moved description of addins more to fun not addin.dcf

--- R/load_r_scripts.R
+++ R/load_r_scripts.R
@@ -1,4 +1,12 @@
 # Rstudio addin to load R scripts and data files.
+#
+# The package devtools has the 'load_all()' that is useful for loading all
+# scripts in the R folder (and the data in the data folder). RStudio's
+# keybinding for (Ctrl-Shift-L) can only be used in a "package" setting.
+# But the tools usefulness extends beyond package development settings. This
+# addin simply wraps the 'load_all()' function and allows you to set a
+# keybinding to it in RStudio.
 load_r_scripts <- function() {
     devtools::load_all()
 }
+
